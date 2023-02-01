bb62272cdd38efa8c17ca140e0ecc307715cdab2
Yaskow <a.yas44@yahoo.fr>
2023-02-01 23:45:50 +0100

    Tp2

--- src/translator.cpp
+++ src/translator.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
+#include <string>
+#include <set>
+#include <map>
 
-int main()
-{
-    while (true)
-    {
+using namespace  std;
+
+std::set<string> make_exit_commands(){
+    return {"e", "exit", "q", "quit"};
+}
+
+int main(){
+    string value;
+    std::string w1;
+    std::string w2;
+    auto set = make_exit_commands();
+    std::map<string, string> ourMap;
+
+    while (true){
         std::cout << "This is a translator, but it does nothing for now..." << std::endl;
-        std::cin.ignore();
+        std::cin >> value;
+        if(set.count(value) > 0){
+            break;
+        }
+        if(value == "add"){
+            std::cin >>  w1;
+            std::cin >> w2;
+            ourMap.emplace(w1,w2);
+            std::cout << w1 +" => "+w2 << endl;
+        }
+        if(value == "translate"){
+            std::cin >> w1;
+            if(!ourMap.contains(w1)){
+
+            }
+        }
     }
 
     return 0;
