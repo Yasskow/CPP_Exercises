aadb37206c79a43739b56a278238675c9f1f2b80
Yaskow <a.yas44@yahoo.fr>
2023-02-27 17:51:59 +0100

    Saved changes.

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -1,1 +1,22 @@
 #pragma once
+
+class Plush
+{
+public:
+    Plush(int cost)
+        : _cost{cost}
+    {
+    }
+
+    void set_cost(int newcost)
+    {
+        _cost = newcost;
+    }
+    int get_cost() const
+    {
+        return _cost;
+    }
+
+private:
+    int _cost = 10;
+};
--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,1 +1,58 @@
 #pragma once
+
+#include <string>
+
+using namespace std;
+
+class PlushStore
+{
+public:
+    PlushStore(string name)
+        : _name{name} {}
+
+    string get_name() const
+    {
+        return _name;
+    }
+    int get_wealth_amount() const
+    {
+        return _wealth;
+    }
+    int get_stock_size() const
+    {
+        return _stock;
+    }
+    void loan(int cost)
+    {
+        _wealth += cost;
+        _debt += cost + (cost / 10);
+    }
+    int get_debt_amount() const
+    {
+        return _debt;
+    }
+    void make_plush(int cost)
+    {
+        if (_wealth - cost < 0)
+        {
+            _wealth = 0;
+            return;
+        }
+        else if (_wealth - cost >= -5 && _wealth - cost <= 0)
+        {
+            _wealth = 0;
+            _stock += 1;
+            return;
+        }
+        _wealth -= cost;
+        _stock += 1;
+        _experience += 1;
+    }
+
+private:
+    string _name;
+    int _wealth = 0;
+    int _stock = 0;
+    int _debt = 0;
+    int _experience = 0;
+};
